3c7c34b70ef6ede48ba56c50bbd18e8dc499dc53
Francesco Lanciana <francescolanciana@gmail.com>
2019-04-14 01:18:54 +1000

    About Page WIP

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Layout from "@components/Layout/Layout";
+
+import "./index.scss";
+
+const AboutPage = () => (
+    <Layout>
+        <div className="about-page">
+            <h1>Pockets: The Story</h1>
+            <p>
+                Also if you have a fashion label or even just an idea for a new clothing item please
+                contact us, we want your products to be in front of{" "}
+            </p>
+        </div>
+    </Layout>
+);
+
+export default AboutPage;
