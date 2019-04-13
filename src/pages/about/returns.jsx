2fa4a54492527378b6435bd9da5c4b36e1ea8e64
Francesco Lanciana <francescolanciana@gmail.com>
2019-04-14 00:29:53 +1000

    Returns Page Filler
    Added a filler for the returns page (needs to be completed)

---
+++ src/pages/about/returns.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "@components/Layout/Layout";
+
+import "./returns.scss";
+
+const ReturnsPage = () => (
+    <Layout>
+        <div className="returns-page">
+            <h1>Returns Policies</h1>
+            <p>
+                NN
+            </p>
+        </div>
+    </Layout>
+);
+
+export default ReturnsPage;
