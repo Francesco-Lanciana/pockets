c59d58c1606a741c24b40b0f27b093f6b9fdb7dc
Francesco Lanciana <francescolanciana@gmail.com>
2019-04-14 00:31:07 +1000

    Added filler for contact page
    Needs to be finished

---
+++ src/pages/about/contact-us.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "@components/Layout/Layout";
+
+import "./contact-us.scss";
+
+const ContactUsPage = () => (
+    <Layout>
+        <div className="contact-us-page">
+            <h1>Contact us</h1>
+            <p>Contacting us is simple, all you need is a</p>
+        </div>
+    </Layout>
+);
+
+export default ContactUsPage;
