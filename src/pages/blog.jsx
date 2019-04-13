2cc2d0bd465f1321f60b0b56dab7ff1b8d764375
Francesco Lanciana <francescolanciana@gmail.com>
2019-04-14 00:30:15 +1000

    Added Filler for Blog Page
    Needs to be completed

---
+++ src/pages/blog.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./blog.scss";
+
+const BlogPage = () => (
+    <div className="blog-page">
+        <h1>Our blog</h1>
+    </div>
+);
+
+export default BlogPage;
