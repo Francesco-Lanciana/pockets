4834facf1923b7f665eecbb9c10d83708b5acb40
Francesco Lanciana <francescolanciana@gmail.com>
2019-04-24 21:15:42 +1000

    Shopping cart container works at most screen widths and heights
    Due to the shopping cart being attached to the navbar which is position sticky, we experienced problems at smaller screen sizes. If the modal went off the screen there was no way to scroll down to it as it was essentially fixed. Also at smaller screen widths text would wrap to the next line causing items to become messy. 
    
    We now have:
    * Max height that prevents the container from going over the viewport
    * A scroll container that makes all content inside it scrollable while allowing for an escape hatch for items inside it that want to position themselves relative to the parent container.
    * The height of the shopping cart psuedo-hardcoded (using css variables to give context about the amount of items and therefore what the height should be). Without this the inner scroll container can't work (it bases its height on its parent).
    * A better box shadow (inspired by youtube)
    * The triangle on top of the modal has a "border" (actually just two psuedo elements on top of each other with one slightly bigger than the other).
    
    This will most likely be reverted because it feels like a stack of cards (Hardcoding the height is very error prone and hacky). Most likely i will implement another solution that is an item preview page for smaller devices (This is far simpler).

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -19,7 +19,7 @@ const ShoppingCart = ({ items }) => {
     const numItems = items.reduce((total, { quantity }) => total + quantity, 0);
     const totalPrice = items.reduce((total, { price, quantity }) => total + price * quantity, 0);
     const currencySymbol = getCurrencySymbol("aud");
-    const qualifiedTotalPrice = `${currencySymbol}${(totalPrice/100).toFixed(2)}`;
+    const qualifiedTotalPrice = `${currencySymbol}${(totalPrice / 100).toFixed(2)}`;
     const cartIsEmpty = items.length === 0;
 
     useEffect(() => {
@@ -45,51 +45,53 @@ const ShoppingCart = ({ items }) => {
         }
     }
 
+    /* I am not proud of the num-items thing... The min of 1 is to cope with the empty message that
+    is shown and the max of 4.5 is to cope with the list only growing to accomodate 4.5 items before
+    requiring that you scroll */
     return (
-        <div className="shopping-cart">
-            <div className="shopping-cart-header">
-                <div className="shopping-cart-icon-container">
-                    <ShoppingCartImage />
+        <div className="shopping-cart" style={{ "--num-items": Math.min((items.length || 1), 4.5) }}>
+            <div className="shopping-cart-inner-scroll">
+                <div className="shopping-cart-header">
+                    <div className="shopping-cart-icon-container">
+                        <ShoppingCartImage />
+                    </div>
+                    <span className="shopping-cart-badge">{numItems}</span>
+                    <div className="shopping-cart-total">
+                        <span className="lighter-text">Total: </span>
+                        <span className="main-color-text">{qualifiedTotalPrice}</span>
+                    </div>
                 </div>
-                <span className="shopping-cart-badge">{numItems}</span>
-                <div className="shopping-cart-total">
-                    <span className="lighter-text">Total: </span>
-                    <span className="main-color-text">{qualifiedTotalPrice}</span>
-                </div>
-            </div>
 
-            <ul className="shopping-cart-items">
-                {items.map((item) => (
-                    <li className="shopping-cart-item">
-                        <div className="clothing-image-container">
-                            <Img
-                                sizes={{ ...item.image.childImageSharp.fluid }}
-                                fluid={item.image.childImageSharp.fluid}
-                                imgStyle={{ objectFit: "contain" }}
-                                className="clothing-image"
-                            />
-                        </div>
-                        <div className="shopping-cart-item-details">
-                            <div className="item-name">{item.name}</div>
-                            <div className="shopping-cart-item-meta-details">
-                                <span className="item-price">{`${currencySymbol}${(item.price /
-                                    100).toFixed(2)}`}</span>
-                                <span className="item-quantity">Quantity: {item.quantity}</span>
+                <ul className="shopping-cart-items">
+                    {items.map((item) => (
+                        <li className="shopping-cart-item">
+                            <div className="clothing-image-container">
+                                <Img
+                                    sizes={{ ...item.image.childImageSharp.fluid }}
+                                    fluid={item.image.childImageSharp.fluid}
+                                    imgStyle={{ objectFit: "contain" }}
+                                    className="clothing-image"
+                                />
                             </div>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+                            <div className="shopping-cart-item-details">
+                                <div className="item-name">{item.name}</div>
+                                <div className="shopping-cart-item-meta-details">
+                                    <span className="item-price">{`${currencySymbol}${(
+                                        item.price / 100
+                                    ).toFixed(2)}`}</span>
+                                    <span className="item-quantity">Quantity: {item.quantity}</span>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
 
-            {cartIsEmpty && (
-                <div className="empty-cart-message">
-                    Only those with carts abrim may be allowed to see the secrets within
-                </div>
-            )}
+                {cartIsEmpty && <div className="empty-cart-message">You cart is currently empty</div>}
 
-            <button className="checkout-btn" onClick={redirectToCheckout} disabled={true}>
-                Checkout
-            </button>
+                <button className="checkout-btn" onClick={redirectToCheckout} disabled={true}>
+                    Checkout
+                </button>
+            </div>
         </div>
     );
 };
