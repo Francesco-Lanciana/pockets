17b8847c6e5211b88912adf49c39d82f8d04e8ad
Francesco Lanciana <francescolanciana@gmail.com>
2019-05-14 00:57:40 +1000

    Global styles for bottom nav to stick to bottom of screen
    Without this our flexbox hack wouldn't work

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,7 @@
 
 // You can delete this file if you're not using it
 import React from "react"
+import "@styles/global.css";
 
 import { ShoppingCartProvider } from "@context/ShoppingCartContext/ShoppingCartContext"
 
