868afe84aa265d8aa600647fb15de1e643dde088
Francesco Lanciana <francescolanciana@gmail.com>
2019-05-19 22:43:33 +1000

    Add supplier data for reference in the context page
    Allows me to show the customer what they are in for

---
+++ src/utils/supplierData.js
@@ -0,0 +1,34 @@
+const metadata = {
+    "asos": {
+        deliveryMin: 2,
+        deliveryMax: 20,
+        returns: "unused",
+        saleItemReturns: true,
+        returnShippingFee: false,
+        international: true,
+    },
+    "the iconic": {
+        deliveryMin: 1,
+        deliveryMax: 5,
+        returns: "unused",
+        saleItemReturns: true,
+        returnShippingFee: false,
+        international: false,
+    },
+    "poche posh": {
+        deliveryMin: 5,
+        deliveryMax: 20,
+        returns: "unused",
+        saleItemReturns: true,
+        returnShippingFee: false,
+        international: true,
+    },
+    "zaful": {
+        deliveryMin: 6,
+        deliveryMax: 25,
+        returns: "warranty",
+        saleItemReturns: false,
+        returnShippingFee: false,
+        international: true,
+    }
+}
