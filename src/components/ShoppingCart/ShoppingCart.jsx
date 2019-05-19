c366896c3c499ebaae5e0a4ded3c39eb28794d33
Francesco Lanciana <francescolanciana@gmail.com>
2019-05-19 22:38:48 +1000

    Remove link to Stripe - replace with checkout page, add quantity changer
    Now that we have the checkout page we don't need the shopping cart to link directly to Stripe, it just goes to the checkout page instead. Also added a quick quantity changer to each item for the users ease of mind (can change things without commiting to checking out).

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -2,58 +2,27 @@
  * TODO: Enable the checkout button once Stripe has been set up
  */
 
-import React, { useEffect } from "react";
+import React, { useContext } from "react";
 import Img from "gatsby-image";
+import { Link } from "gatsby";
 
+import ShoppingCartContext from "@context/ShoppingCartContext/ShoppingCartContext";
 import { getCurrencySymbol } from "@utils/currencyHelpers";
 import ShoppingCartImage from "@images/shopping-cart-solid.svg";
-import StripeApi from "@api/stripeApi";
 
 import "./ShoppingCart.scss";
 
-/* We want this to persist across the shopping carts lifecycle (i.e. unmounts) as every time we
-instantiate window.Stripe it creates another thread, and without this we would instantiate a thread
-each time this component mounts */
-let stripe;
-
 const ShoppingCart = ({ items }) => {
+    const { changeItemQuantity } = useContext(ShoppingCartContext);
     const numItems = items.reduce((total, { quantity }) => total + quantity, 0);
     const totalPrice = items.reduce((total, { price, quantity }) => total + price * quantity, 0);
     const currencySymbol = getCurrencySymbol("aud");
     const qualifiedTotalPrice = `${currencySymbol}${(totalPrice / 100).toFixed(2)}`;
     const cartIsEmpty = items.length === 0;
 
-    useEffect(() => {
-        stripe =
-            stripe ||
-            window.Stripe("pk_test_GMu2GHlqqnAFu9bWQwEYCkQG00Z3HIFFSj", {
-                betas: ["checkout_beta_4"],
-            });
-    }, []);
-
-    async function redirectToCheckout(event) {
-        event.preventDefault();
-        const itemsToCharge = items.map(({ id: sku, quantity }) => ({ sku, quantity }));
-        const skuIds = itemsToCharge.map(({ sku }) => sku);
-        const quantities = itemsToCharge.reduce((quantities, item) => {
-            return { ...quantities, [item.sku]: item.quantity }
-        }, {});
-        const [err, { session }] = await StripeApi.createSession(skuIds, quantities);
-
-        if (err) {
-            console.log("Error: ", err);
-        }
-
-        const { error } = await stripe.redirectToCheckout({
-            //items: itemsToCharge,
-            sessionId: session.id,
-            // successUrl: `http://localhost:8000/page-2/`,
-            // cancelUrl: `http://localhost:8000/`,
-        });
-
-        if (error) {
-            console.warn("Error:", error);
-        }
+    function handleQuantityChange(id, add) {
+        if (add) changeItemQuantity(id, (quantity) => quantity + 1);
+        if (!add) changeItemQuantity(id, (quantity) => quantity - 1);
     }
 
     /* I am not proud of the num-items thing... The min of 1 is to cope with the empty message that
@@ -75,7 +44,7 @@ const ShoppingCart = ({ items }) => {
             {!!items.length && (
                 <ul className="shopping-cart-items">
                     {items.map((item) => (
-                        <li className="shopping-cart-item">
+                        <li className="shopping-cart-item" key={item.id}>
                             <div className="clothing-image-container">
                                 <Img
                                     sizes={{ ...item.image.childImageSharp.fluid }}
@@ -85,12 +54,16 @@ const ShoppingCart = ({ items }) => {
                                 />
                             </div>
                             <div className="shopping-cart-item-details">
-                                <div className="item-name">{item.name}</div>
+                                <div className="item-name">{`${item.name} - ${item.size}`}</div>
                                 <div className="shopping-cart-item-meta-details">
                                     <span className="item-price">{`${currencySymbol}${(
                                         item.price / 100
                                     ).toFixed(2)}`}</span>
-                                    <span className="item-quantity">Quantity: {item.quantity}</span>
+                                    <div className="quantity-changer" data-quantity={item.quantity}>
+                                        <button className="quantity-change-btn minus" onClick={() => handleQuantityChange(item.id, false)}></button>
+                                        <span className="quantity">{item.quantity}</span>
+                                        <button className="quantity-change-btn plus" onClick={() => handleQuantityChange(item.id, true)}></button>
+                                    </div>
                                 </div>
                             </div>
                         </li>
@@ -100,9 +73,9 @@ const ShoppingCart = ({ items }) => {
 
             {cartIsEmpty && <div className="empty-cart-message">You cart is currently empty</div>}
 
-            <button className="checkout-btn" onClick={redirectToCheckout} disabled={cartIsEmpty}>
-                Checkout
-            </button>
+            <Link to={`/checkout`} className="checkout-link" onClick={() => { document.body.style.overflow = "visible" }}>
+                Go To Checkout
+            </Link>
         </div>
     );
 };
