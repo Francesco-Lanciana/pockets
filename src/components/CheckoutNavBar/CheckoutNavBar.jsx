f37b740a6706551342134f394a4d719f67b56c76
Francesco Lanciana <francescolanciana@gmail.com>
2019-05-20 21:51:00 +1000

    Centered logo, added back arrow to continue shopping link
    The logo was currently in the middle of the two links, however its position depended on the size of the links, if one was different to the other it wouldn't be centered.

--- src/components/CheckoutNavBar/CheckoutNavBar.jsx
+++ src/components/CheckoutNavBar/CheckoutNavBar.jsx
@@ -3,6 +3,8 @@ import { Link } from "gatsby";
 import { useMedia } from "@hooks/window-hooks";
 
 import logo from "@images/logo-semi.png";
+import BackArrow from "@images/long-arrow-left-light.svg";
+
 import "./CheckoutNavBar.scss";
 
 const CheckoutNavBar = () => {
@@ -12,13 +14,14 @@ const CheckoutNavBar = () => {
         <header className="checkout-nav-bar">
             <div className="navbar-content">
                 <Link to="/" className="back-link">
-                    <span>{smallScreen ? "Back" : "Continue Shopping"}</span>
+                    <span className="back-arrow-container"><BackArrow /></span>
+                    <span className="navbar-link-text">{smallScreen ? "Back" : "Continue Shopping"}</span>
                 </Link>
                 <Link to="/" className="logo-container">
                     <img src={logo} className="logo" />
                 </Link>
                 <Link to="/about/returns" className="return-policy-link">
-                    <span>Returns policy</span>
+                    <span className="navbar-link-text">{smallScreen ? "Returns" : "Returns policy"}</span>
                 </Link>
             </div>
         </header>
